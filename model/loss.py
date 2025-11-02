681c5000a1f524d769bcb1a86090fcdfa3c44705
helkun <farflylonglight@gmail.com>
2025-11-02 23:14:48 +0900

    loss計算の修正

--- model/loss.py
+++ model/loss.py
@@ -31,25 +31,11 @@ class ParamsLoss(nn.Module):
         cont_pred,
         cont_target
     ):
-        cont_loss = 0.0
-
-        for name in cont_target.keys():
-            pred = cont_pred[name]
-            target = cont_target[name]
-            cont_loss += self.cont_loss_fn(pred, target)
-        cont_loss /= len(cont_target)
+        cont_loss = self.cont_loss_fn(cont_pred, cont_target)
 
         # Categorical Lossの計算
-        categ_loss = 0.0
-        for i, param_name in enumerate(categ_target.keys()):
-            loss_fn = self.categ_loss_fns[param_name] if self.categ_class_weights is not None else self.categ_loss_fn
-            pred = categ_pred[param_name]
-            target = categ_target[param_name]
-            loss = loss_fn(pred, target)
-            if self.categ_param_weight is not None and param_name in self.categ_param_weight:
-                loss = loss * self.categ_param_weight[param_name]
-            categ_loss += loss.mean()
-        categ_loss /= len(categ_target)
+        categ_loss = self.categ_loss_fn(categ_pred, categ_target)
+        categ_loss = categ_loss.mean()
 
         total_loss = self.cont_weight * cont_loss + self.categ_weight * categ_loss
         return total_loss, self.cont_weight * cont_loss, self.categ_weight * categ_loss
